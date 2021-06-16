3a2a7664ec22881d18df31b44e58fe79253b9004
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-06-16 00:06:56 +0000

    Comentário no sleep

--- proc-filho/main.go
+++ proc-filho/main.go
@@ -8,5 +8,5 @@ import (
 
 func main() {
 	fmt.Printf("Olá, sou processo filho %d. Meu pai é %d\n", os.Getpid(), os.Getppid())
-	time.Sleep(10 * time.Second)
+	time.Sleep(10 * time.Second) // Dorme por 10 segundos.
 }
