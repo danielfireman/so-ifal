e9c3389fa71ed50e4c80bc08d190f97b4e6ff3ad
Daniel Fireman <danielfireman@gmail.com>
2021-07-20 20:37:01 -0300

    melhorando clareza no exemplo de Semaforo

--- coordenacao_tarefas/Semaforo.java
+++ coordenacao_tarefas/Semaforo.java
@@ -1,8 +1,8 @@
 import java.util.concurrent.Semaphore;
 
 public class Semaforo {
-    static final int nThreads = 100;
-    static final int nSteps = 10000;
+    static final int nThreads = 10;
+    static final int nSteps = 10;
     static int sum = 0;
     static final Semaphore sem = new Semaphore(1);
 
@@ -11,21 +11,7 @@ public class Semaforo {
 
         // Cria todas as threads.
         for (int i = 0; i < nThreads; i++) {
-            threads[i] = new Thread(() -> {
-                // Corpo da thread.
-                for (int j = 0; j < nSteps; j++) {
-                    try {
-                        sem.acquire();
-                        // Refletir sobre o overhead gerado pela região onde está a região
-                        // crítica.
-                        sum = sum + 1; // região crítica (existe condição de corrida)
-                        sem.release();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        System.exit(-1);
-                    }
-                }
-            });
+            threads[i] = new IncrementaThread(i);
         }
 
         // Inicializa as thread.
@@ -39,5 +25,33 @@ public class Semaforo {
         }
 
         System.out.println(sum);
+    }
+
+    static class IncrementaThread extends Thread {
+        int id;
+
+        IncrementaThread(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            // Corpo da thread.
+            for (int j = 0; j < nSteps; j++) {
+                try {
+                    System.out.printf("[%d] pedindo para entrar na região crítica.\n", id);
+                    sem.acquire();
+                    System.out.printf("[%d] dentro região crítica.\n", id);
+                    // Refletir sobre o overhead gerado pela região onde está a região
+                    // crítica.
+                    sum = sum + 1; // região crítica (existe condição de corrida)
+                    sem.release();
+                    System.out.printf("[%d] fora da região crítica.\n", id);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    System.exit(-1);
+                }
+            }
+        }
     }
 }
