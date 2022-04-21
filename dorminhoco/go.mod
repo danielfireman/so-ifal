58028e589c975028b5a235516e697f9fa69dfbb2
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2022-04-21 12:04:43 +0000

    adiciona dorminhoco

---
+++ dorminhoco/go.mod
@@ -0,0 +1,3 @@
+module dorminhoco
+
+go 1.17
---
+++ dorminhoco/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Irei dormir por 30 segundos")
+	time.Sleep(30 * time.Second)
+	fmt.Println("Acordando ...")
+}
