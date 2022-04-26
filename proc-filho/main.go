4edf04b4610626476ebc46dcb1396e0ecf99d4df
Daniel Fireman <danielfireman@gmail.com>
2022-04-26 20:47:54 +0000

    Renomeia comando proc-pai e adicionada chamada explícita a proc-filho

--- proc-filho/main.go
+++ proc-filho/main.go
@@ -9,4 +9,5 @@ import (
 func main() {
 	fmt.Printf("Olá, sou processo filho %d. Meu pai é %d\n", os.Getpid(), os.Getppid())
 	time.Sleep(10 * time.Second) // Dorme por 10 segundos.
+	os.Exit(0)                   // Essa chamada poderia ser omitida.
 }
